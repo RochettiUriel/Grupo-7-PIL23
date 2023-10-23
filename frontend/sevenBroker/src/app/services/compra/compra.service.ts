285254e1d5ad114bf2c0560af7b3b664cd9affb6
matias angelaccio <matiasangelaccio03@gmail.com>
2023-10-23 18:49:47 -0300

    agregue servicio

--- frontend/sevenBroker/src/app/services/compra/compra.service.ts
+++ frontend/sevenBroker/src/app/services/compra/compra.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class CompraService {
-  private datosCompra: any;
 
-  setDatosCompra(datos: any) {
-    this.datosCompra = datos;
+  constructor(private http: HttpClient) { }
+
+  url : string = "http://localhost:5124/"
+
+  post(objeto : any): Observable<any>{
+    return this.http.post(this.url + "api/Compra", objeto);
   }
 
-  getDatosCompra() {
-    return this.datosCompra;
+  get(id? : number): Observable<any>{
+    if(id !== undefined){
+
+      return this.http.get(this.url + `api/Compra/${id}`);
+
+    } else {
+
+      return this.http.get(this.url + "api/Compra")
+
+    }
   }
 }
